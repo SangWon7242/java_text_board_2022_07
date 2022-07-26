467744ac14db1369ab567cf9e83f8d7163d45b5b
SangWon7242 <gotkdqja@gmail.com>
2022-07-26 20:22:09 +0900

    회원 가입 기능 구현

--- src/com/psw/exam/board/Container.java
+++ src/com/psw/exam/board/Container.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Container {
   static Scanner sc;
   static UsrArticleController usrArticleController;
+  static UsrMemberController usrMemberController;
 
   static {
     sc = new Scanner(System.in);
     usrArticleController = new UsrArticleController();
+    usrMemberController = new UsrMemberController();
   }
 
 }
---
+++ src/com/psw/exam/board/Member.java
@@ -0,0 +1,20 @@
+package com.psw.exam.board;
+
+public class Member {
+  int id;
+  String loginId;
+  String loginPw;
+
+  String 테스트;
+
+  Member(int id, String loginId, String loginPw) {
+    this.id = id;
+    this.loginId = loginId;
+    this.loginPw = loginPw;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, loginId, loginPw);
+  }
+}
