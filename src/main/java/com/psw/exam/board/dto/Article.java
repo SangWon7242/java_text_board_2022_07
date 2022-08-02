80c6db0635603abded61701f909ba933e417fb09
SangWon7242 <gotkdqja@gmail.com>
2022-08-02 21:14:58 +0900

    Article 클래스에 boardId 필드 추가

--- src/main/java/com/psw/exam/board/dto/Article.java
+++ src/main/java/com/psw/exam/board/dto/Article.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Article {
   private int id;
+  private int boardId;
   private String title;
   private String body;
-
 }
